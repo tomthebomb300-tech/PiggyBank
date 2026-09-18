7a93bd60edc3ffadf0a6ba4faae56b66692aec2f
Thomas O' Callaghan <tomthebomb300@gmail.com>
2026-09-18 21:24:11 +0100

    Added tooltip to candle stick chart

--- View/Transactions/candle_stick_chart.py
+++ View/Transactions/candle_stick_chart.py
@@ -9,11 +9,11 @@ class Candle_stick_chart(ctk.CTkFrame):
         super().__init__(parent, fg_color=fg_color, corner_radius=corner_radius)
         self.controller = controller
 
-        ohlc = self.controller.getOHLC("ME")
-        print(ohlc)
-
+        self.timeframe = "W"
+        self.ohlc = self.controller.getOHLC(self.timeframe)
+   
         self.create_chart(self)
-        self.update_chart(ohlc)
+        self.update_chart()
 
     def create_chart(self, parent):
         self.fig = Figure(figsize = (6, 3), dpi = 100, layout="tight")
@@ -25,10 +25,76 @@ class Candle_stick_chart(ctk.CTkFrame):
         self.canvas = FigureCanvasTkAgg(self.fig, parent)
         self.canvas.get_tk_widget().pack(fill = "both", expand = True, pady=(0,40))
 
-    def update_chart(self, ohlc):
+    def update_chart(self):
+        self.ax.clear()
+        self.add_tooltip()
+
         mpf.plot(
-            ohlc,
+            self.ohlc,
             type="candle",
             style="charles",
             ax=self.ax
         )
+
+    def add_tooltip(self):
+            self.vline = self.ax.axvline(x = 0, color = "#666666", linestyle = "--", alpha = 0.5)
+            self.vline.set_visible(False)
+
+            self.tooltip = self.ax.text(0.80, 0.95, "", transform = self.ax.transAxes ,ha = "left", va ="top",fontsize=10,
+                    bbox=dict(  boxstyle="round,pad=0.5",
+                                facecolor="#2b2b2b",
+                                edgecolor="white",
+                                alpha=0.9
+                            ),
+                color="white"
+            )
+            self.tooltip.set_visible(False)
+            self.canvas.mpl_connect("motion_notify_event",self.on_hover)
+
+    def on_hover(self, event):
+        if(event.inaxes != self.ax or event.xdata is None):
+            self.hide_toolbar()
+            return
+
+        index = int(round(event.xdata))
+        if(index >= len(self.ohlc) or index < 0):
+            self.hide_toolbar()
+            return
+
+        self.vline.set_xdata([index, index])
+        self.vline.set_visible(True)
+
+
+        self.tooltip.set_text("{0}".format(
+            self.get_text(self.ohlc.index.tolist()[index].date(), self.ohlc.iloc[index])
+        ))
+
+        self.tooltip.set_visible(True)
+        self.canvas.draw_idle()
+
+
+    def hide_toolbar(self):
+        self.vline.set_visible(False)
+        self.tooltip.set_visible(False)
+        self.canvas.draw_idle()
+
+    def get_text(self, date, ohlc):
+        open = f"{ohlc.open:,}"
+        high = f"{ohlc.high:,}"
+        low = f"{ohlc.low:,}"
+        close = f"{ohlc.close:,}"
+        
+        if(self.timeframe == "D"):
+            formatted_date = date.strftime('%d / %b / %Y')
+            day = date.strftime("%A")
+            return "{0}\n{1}\n\nO: {2}\nH: {3}\nL: {4}\nC: {5}".format(formatted_date, day, open, high, low, close)
+        elif(self.timeframe == "W"):
+            month_year = date.strftime("%b / %Y")
+            week = date.strftime("%W")
+            return "{0}\nWeek: {1}\n\nO: {2}\nH: {3}\nL: {4}\nC: {5}".format(month_year, week, open, high, low, close)
+        elif(self.timeframe == "ME"):
+            month_year = date.strftime("%b / %Y")
+            return "{0}\n\nO: {1}\nH: {2}\nL: {3}\nC: {4}".format(month_year, open, high, low, close)
+        elif(self.timeframe == "YE"):
+            year = date.strftime("%Y")
+            return "{0}\n\nO: {1}\nH: {2}\nL: {3}\nC: {4}".format(year, open, high, low, close)
