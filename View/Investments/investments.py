14c26d5592a2ced39ad332f75813bc93dc8693bd
Thomas O' Callaghan <tomthebomb300@gmail.com>
2026-09-26 03:08:55 +0100

    Added overview section at top investments page.

--- View/Investments/investments.py
+++ View/Investments/investments.py
@@ -10,11 +10,38 @@ class Investments(ctk.CTkFrame):
         super().__init__(parent, fg_color=fg_color, corner_radius=corner_radius)
         self.controller = controller
 
-        frame = ctk.CTkFrame(self, fg_color="#1d2228", corner_radius=40)
-        frame.pack(fill = "both", expand = True, padx = 10, pady = (10,10))
-        self.create_chart(frame)
         dates, deposits, portfolio_value = self.controller.getDatesDepositsPortValue()
+
+        frame = ctk.CTkFrame(self, fg_color="transparent", corner_radius=40)
+        frame.pack(fill = "both", expand = True, padx = 20, pady = 20)
+
+        self.create_chart(frame)
+        self.create_overview(frame, deposits, portfolio_value)
         self.update_chart(dates, deposits, portfolio_value)
+
+
+    def create_overview(self, parent, deposits, portfolio_value):
+        overview = ctk.CTkFrame(parent, fg_color="transparent", corner_radius=40)
+        overview.pack(side="top", anchor = "n")
+
+        deposited = deposits[len(deposits)-1]
+        value = portfolio_value[len(portfolio_value)-1]
+
+        self.create_card(overview, "Deposited", round(deposited,2))
+        self.create_card(overview, "Value", round(value,2))
+        self.create_card(overview, "Un-realized P&L", round(value-deposited,2))
+
+
+    def create_card(self, parent, title, value):
+        card = ctk.CTkFrame(parent, fg_color="#1d2228", corner_radius=40)
+        card.pack(side="left", padx = 10, pady = 10)
+
+        title_label = ctk.CTkLabel(card,text=title,font=("Arial", 20),text_color="white")
+        title_label.pack(anchor="w",padx=20,pady=(20,0))
+
+        value_label = ctk.CTkLabel(card,text=value,font=("Arial", 32),text_color="white")
+        value_label.pack(anchor="w",padx=20,pady=(20,20))
+
 
     def create_chart(self, parent):
         self.fig = Figure(figsize = (6, 3), dpi = 100, layout="tight")
@@ -24,7 +51,7 @@ class Investments(ctk.CTkFrame):
         self.ax.set_facecolor("#1d2228")
 
         self.canvas = FigureCanvasTkAgg(self.fig, parent)
-        self.canvas.get_tk_widget().pack(side = "bottom", fill = "both", expand = True, pady=(40,40))   
+        self.canvas.get_tk_widget().pack(side = "bottom", fill = "both", expand = True, pady=(20,40))   
 
     def update_chart(self, dates, deposits, portfolio_value):
         self.ax.clear()
